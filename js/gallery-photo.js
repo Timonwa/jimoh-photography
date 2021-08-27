299e8f44db0636920b83e1f3d0f963f97b1842e7
Pelumi Eyitimonwa Akintokun <pelumiakintokun@gmail.com>
2021-08-27 20:43:18 +0100

    added the data for the images that will be viewed in the gallery.

---
+++ js/gallery-photo.js
@@ -0,0 +1,36 @@
+// ! album link names that will be visible on the menu as dropdown for gallery link
+const albumLinks = ['Album 1', 'Album 2'];
+
+// ! Images for album 1
+const album1Images = [
+    { albumName: "Album 1" },
+    "/img/album1/photo1.jpg",
+    "/img/album1/photo2.jpg",
+    "/img/album1/photo3.jpg",
+    "/img/album1/photo4.jpg",
+    "/img/album1/photo1.jpg",
+    "/img/album1/photo2.jpg",
+    "/img/album1/photo3.jpg",
+    "/img/album1/photo4.jpg",
+    "/img/album1/photo1.jpg",
+    "/img/album1/photo2.jpg",
+    "/img/album1/photo3.jpg",
+    "/img/album1/photo4.jpg"
+];
+
+// ! Images for album 2
+const album2Images = [
+    { albumName: "Album 2" },
+    "/img/album2/photo1.jpg",
+    "/img/album2/photo2.jpg",
+    "/img/album2/photo3.jpg",
+    "/img/album2/photo4.jpg",
+    "/img/album2/photo1.jpg",
+    "/img/album2/photo2.jpg",
+    "/img/album2/photo3.jpg",
+    "/img/album2/photo4.jpg",
+    "/img/album2/photo1.jpg",
+    "/img/album2/photo2.jpg",
+    "/img/album2/photo3.jpg",
+    "/img/album2/photo4.jpg"
+];
