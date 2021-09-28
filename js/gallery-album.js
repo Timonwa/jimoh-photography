68295b22bd8824df331ef84611d49aef25ddee2e
Pelumi Eyitimonwa Akintokun <pelumiakintokun@gmail.com>
2021-09-28 13:10:20 +0100

    added modal popup on all images, and refactored the gallery codes.

---
+++ js/gallery-album.js
@@ -0,0 +1,29 @@
+import { galleryAlbumImages } from "./photos.js";
+
+//* the gallery album
+// select album cntr
+const albumCntr = document.querySelector('.gallery-album');
+
+// function to print out all the album images into the album and unto the screen
+for (var i = 0; i < galleryAlbumImages.length; i++) {
+    albumCntr.innerHTML += '<span><img class="album-image" src="' + galleryAlbumImages[i][0] + '" alt="' + galleryAlbumImages[i][1] + '"></span>'
+}
+
+//* the modal popup
+// function to add modal popup to all the images
+// when you click on an image in the gallery, a popup would appear showing the enlarged image
+var individualImage = document.querySelectorAll('.album-image');
+for (i of individualImage) {
+    i.addEventListener('click', function () {
+        document.querySelector(".modal-popup").style.display = "flex";
+        image.src = this.src;
+        image.alt = this.alt;
+    });
+}
+
+// function to close modal popup
+const closeModal = document.querySelector('.close-modal');
+closeModal.addEventListener('click',
+    function () {
+        document.querySelector(".modal-popup").style.display = "none";
+    })
