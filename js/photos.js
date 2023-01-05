e1a7d528c5888f6a2362bb8085115b0906934ab0
Pelumi Eyitimonwa Akintokun <pelumiakintokun@gmail.com>
2023-01-05 14:20:51 +0100

    looks better now.

--- js/photos.js
+++ js/photos.js
@@ -3,26 +3,15 @@
 
 // for home album
 export const homeAlbumImages = [
-    ["../../img/featured-photos/photo1.jpg", "image 1"],
-    ["../../img/featured-photos/photo2.jpg", "image 2"],
-    ["../../img/featured-photos/photo3.jpg", "image 3"],
-    ["../../img/featured-photos/photo4.jpg", "image 4"],
-    ["../../img/featured-photos/photo1.jpg", "image 1"],
-    ["../../img/featured-photos/photo2.jpg", "image 2"],
-    ["../../img/featured-photos/photo3.jpg", "image 3"],
-    ["../../img/featured-photos/photo4.jpg", "image 4"]
-];
-
-// for gallery album
-export const galleryAlbumImages = [
-    ["../../img/featured-photos/photo1.jpg", "image 1"],
-    ["../../img/featured-photos/photo2.jpg", "image 2"],
-    ["../../img/featured-photos/photo3.jpg", "image 3"],
-    ["../../img/featured-photos/photo4.jpg", "image 4"],
-    ["../../img/featured-photos/photo1.jpg", "image 5"],
-    ["../../img/featured-photos/photo2.jpg", "image 6"],
-    ["../../img/featured-photos/photo3.jpg", "image 7"],
-    ["../../img/featured-photos/photo3.jpg", "image 8"],
-    ["../../img/featured-photos/photo3.jpg", "image 9"],
-    ["../../img/featured-photos/photo4.jpg", "image 10"]
+  ["../../img/photo1.jpg", "image 1"],
+  ["../../img/photo2.jpg", "image 2"],
+  ["../../img/photo3.jpg", "image 3"],
+  ["../../img/photo4.jpg", "image 4"],
+  ["../../img/image5.jpg", "image 5"],
+  ["../../img/image6.jpg", "image 6"],
+  ["../../img/image7.jpg", "image 7"],
+  ["../../img/image8.jpg", "image 8"],
+  ["../../img/image9.jpg", "image 9"],
+  ["../../img/image10.jpg", "image 10"],
+  ["../../img/image11.jpg", "image 11"],
 ];
